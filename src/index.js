439b5d76ec3c13a6b8552a66122c9d83c7c804c5
t8822_000 <t882200@gmail.com>
2019-04-07 21:36:46 +0300

    basic setup - initial commit

---
+++ src/index.js
@@ -0,0 +1,78 @@
+// render engine
+const InitDOM = () => {
+
+}
+
+const Render = (str) => {
+  // example component:
+  // <h1>Hello</h1>
+  
+  // example nested component:
+  // <div><h1>Hello</h1></div>
+
+  // search starting tag
+  let startingStart = str.search("<");
+  let startingEnd = str.search(">");
+
+  // search ending tag
+  str.
+
+  // take inner HTML and re-parse it
+  str.
+
+  // parse starting tag properties
+  str.
+
+  // handle child component
+  str.
+
+}
+
+class Cmpnt (){
+  state = {
+
+  }
+
+
+}
+
+
+// game engine
+
+// canvas engine
+
+// sound engine
+
+// pdf engine
+
+// text engine
+
+// AI engine
+
+// ML engine
+
+// math engine - wasm+go\rust
+
+// nlp engine
+
+// quick prototype engine
+
+// color eng
+
+// event eng
+
+// data json eng
+
+// image engine
+
+// io engine
+
+// typography engine
+
+// type checking engine
+
+// utils eng
+
+// webgl eng
+
+// core eng
